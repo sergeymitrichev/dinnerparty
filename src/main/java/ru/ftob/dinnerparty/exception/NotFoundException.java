fa27b962d18936e7d56f362b90898b3b3a117db6
sergeymitrichev <sergeymitrichev@yandex.ru>
2018-05-21 00:55:07 +0400

    Add NotFoundException class

---
+++ src/main/java/ru/ftob/dinnerparty/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package ru.ftob.dinnerparty.exception;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
