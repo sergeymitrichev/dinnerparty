2693d6ea20640bbb440e5753df42ec002e005539
sergeymitrichev <sergeymitrichev@yandex.ru>
2018-05-21 11:02:48 +0400

    Configure DB

---
+++ src/main/resources/db/populateDB.sql
@@ -0,0 +1,12 @@
+DELETE FROM user_roles;
+DELETE FROM users;
+ALTER SEQUENCE global_seq RESTART WITH 100000;
+
+INSERT INTO users (email, password) VALUES
+  ('user@yandex.ru', 'password'),
+  ('admin@gmail.com', 'admin');
+
+INSERT INTO user_roles (role, user_id) VALUES
+  ('ROLE_USER', 100000),
+  ('ROLE_ADMIN', 100001),
+  ('ROLE_USER', 100001);
