8955c6164b28b547a7e05bc940f6a4668926a8f9
sergeymitrichev <sergeymitrichev@yandex.ru>
2018-05-24 10:44:25 +0400

    Add DB init and populate scripts

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,16 +1,22 @@
 DROP TABLE IF EXISTS user_roles;
+DROP TABLE IF EXISTS lunch_votes;
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS dishes;
+DROP TABLE IF EXISTS lunches;
+DROP TABLE IF EXISTS restaurants;
 DROP SEQUENCE IF EXISTS global_seq;
 
-CREATE SEQUENCE global_seq START 100000;
+CREATE SEQUENCE global_seq
+  START 100000;
 
 CREATE TABLE users
 (
-  id               INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
-  email            VARCHAR                 NOT NULL,
-  password         VARCHAR                 NOT NULL
+  id       INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+  email    VARCHAR NOT NULL,
+  password VARCHAR NOT NULL
 );
-CREATE UNIQUE INDEX users_unique_email_idx ON users (email);
+CREATE UNIQUE INDEX users_unique_email_idx
+  ON users (email);
 
 CREATE TABLE user_roles
 (
@@ -19,3 +25,45 @@ CREATE TABLE user_roles
   CONSTRAINT user_roles_idx UNIQUE (user_id, role),
   FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
+
+CREATE TABLE restaurants
+(
+  id   INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+  name VARCHAR(255) NOT NULL
+);
+CREATE UNIQUE INDEX restaurants_unique_name_idx
+  ON restaurants (name);
+
+CREATE TABLE lunches
+(
+  id            INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+  date          TIMESTAMP               NOT NULL,
+  restaurant_id INTEGER                 NOT NULL,
+  enabled       BOOL DEFAULT TRUE       NOT NULL,
+  CONSTRAINT lunches_idx UNIQUE (restaurant_id, date),
+  FOREIGN KEY (restaurant_id) REFERENCES restaurants (id) ON DELETE CASCADE
+);
+CREATE UNIQUE INDEX lunches_unique_restaurant_date_idx
+  ON lunches (restaurant_id, date);
+
+CREATE TABLE dishes
+(
+  id       INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+  name     VARCHAR(255) NOT NULL,
+  price    INTEGER      NOT NULL,
+  lunch_id INTEGER      NOT NULL,
+  FOREIGN KEY (lunch_id) REFERENCES lunches (id) ON DELETE CASCADE
+);
+CREATE UNIQUE INDEX dishes_unique_name_lunch_idx
+  ON dishes (name, lunch_id);
+
+CREATE TABLE lunch_votes
+(
+  lunch_id   INTEGER   NOT NULL,
+  lunch_date TIMESTAMP NOT NULL,
+  user_id    INTEGER   NOT NULL,
+  CONSTRAINT lunch_votes_idx UNIQUE (lunch_date, user_id),
+  FOREIGN KEY (lunch_id) REFERENCES lunches (id) ON DELETE CASCADE,
+  FOREIGN KEY (lunch_date) REFERENCES lunches (date) ON DELETE CASCADE,
+  FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+)
--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,7 +1,9 @@
 DELETE FROM user_roles;
 DELETE FROM users;
+DELETE FROM dishes;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
+-- ID 1,2
 INSERT INTO users (email, password) VALUES
   ('user@yandex.ru', 'password'),
   ('admin@gmail.com', 'adminpas');
@@ -10,3 +12,22 @@ INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001),
   ('ROLE_USER', 100001);
+
+-- ID 3,4
+INSERT INTO restaurants (name) VALUES
+  ('Ferris'),
+  ('Papa Potato');
+
+-- ID 5,6
+INSERT INTO lunches(date, restaurant_id) VALUES
+  ('2018-05-30 13:00:00', 3);
+  ('2018-05-30 13:00:00', 4);
+
+-- ID 5,6
+INSERT INTO dishes (name, price, lunch_id) VALUES
+  ('milk',5, 5),
+  ('soup',15, 6);
+
+INSERT INTO lunch_votes (lunch_id, lunch_date, user_id) VALUES
+  (5,'2018-05-30 13:00:00', 1),
+  (6,'2018-05-30 13:00:00', 2);
